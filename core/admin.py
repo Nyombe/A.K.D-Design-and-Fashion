555a936061a671da4489af8d723d40add775490d
Nyombe <acholkuoldeng55@gmail.com>
2026-05-31 12:36:42 -0700

    Fix admin dashboard links: use Django template url tag instead of context variables

--- core/admin.py
+++ core/admin.py
@@ -2,17 +2,17 @@
 Core app admin configuration.
 """
 from django.contrib import admin
-from django.urls import NoReverseMatch, reverse
 
 # Customize the default admin site
 admin.site.site_header = "A.K.D FASHION AND DESIGN Management"
 admin.site.site_title = "A.K.D Management"
 admin.site.index_title = "Welcome to the A.K.D Admin Portal"
+admin.site.index_template = 'admin/jazzmin/dashboard.html'
 
-# Dashboard overview counts
+# Add dashboard context
 _original_index = admin.site.index
 
-def _custom_admin_index(request, extra_context=None):
+def _dashboard_index(request, extra_context=None):
     from users.models import CustomUser
     from products.models import Product
     from orders.models import Order
@@ -35,33 +35,9 @@ def _custom_admin_index(request, extra_context=None):
             'payments': {'count': 0},
         })
 
-    dashboard_urls = {}
-    admin_link_map = {
-        'users_changelist': (CustomUser, 'changelist'),
-        'users_add': (CustomUser, 'add'),
-        'products_changelist': (Product, 'changelist'),
-        'products_add': (Product, 'add'),
-        'orders_changelist': (Order, 'changelist'),
-        'payments_changelist': (Payment, 'changelist'),
-    }
-
-    for key, (model, action) in admin_link_map.items():
-        try:
-            opts = model._meta
-            dashboard_urls[key] = reverse(f'admin:{opts.app_label}_{opts.model_name}_{action}')
-        except (NoReverseMatch, Exception):
-            dashboard_urls[key] = '#'
-
-    try:
-        dashboard_urls['analytics_dashboard'] = reverse('analytics:dashboard')
-    except NoReverseMatch:
-        dashboard_urls['analytics_dashboard'] = '#'
-
-    extra_context.setdefault('dashboard_urls', dashboard_urls)
     return _original_index(request, extra_context)
 
-admin.site.index = _custom_admin_index
-admin.site.index_template = 'admin/jazzmin/dashboard.html'
+admin.site.index = _dashboard_index
 
 
 
